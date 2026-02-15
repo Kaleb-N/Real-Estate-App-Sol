684cd2777b90bfb2a4b4d76a0af6b7c13e288b7a
kaleb-n <nwachukwukaleb@gmail.com>
2026-02-15 12:03:32 +0100

    contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,5 +10,120 @@ interface IERC721 {
 }
 
 contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
 
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
+    }
+
+    modifier onlyInspector() {
+        require(msg.sender == inspector, "Only inspector can call this method");
+        _;
+    }
+
+    // update the lising status
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+    mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    // list a property
+    function list(
+        uint256 _nftID, 
+        address _buyer,
+        uint256 _purchasePrice, 
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        // transfer NFT ownership to escrow contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        // update listing status
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+    }
+
+    // put under contract (only buyer - payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+    // update inspection status (only inspector)
+    function updateInspectionStatus(uint256 _nftID, bool _passed) public onlyInspector{
+        inspectionPassed[_nftID] = _passed;
+    }
+
+    // approve sale (buyer, seller, lender)
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
+    }
+
+    // finalize sale
+    // -> require inspection status (add more items here, like approvals)
+    // -> require sales to be authorized
+    // -> require funds to be correct amount
+    // -> transfer NFT to buyer
+    // -> transfer funds to seller
+    function finalizeSale(uint256 _nftID) public {
+        require(inspectionPassed[_nftID]);
+        require(approval[_nftID][buyer[_nftID]]);
+        require(approval[_nftID][seller]);
+        require(approval[_nftID][lender]);
+        require(address(this).balance >= purchasePrice[_nftID]);
+
+        // pay seller
+        (bool success, ) = payable(seller).call{value: address(this).balance}("");
+        require(success, "Transfer to seller failed");
+
+        // transfer NFT to buyer
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+
+        // update listing status
+        isListed[_nftID] = false;
+    }
+
+    // cancel sale (handle earnest funds)
+    // -> if inspection status is not approved, refund buyer otherwise pay seller
+    function cancelSale(uint256 _nftID) public {
+        if (inspectionPassed[_nftID] == false) {
+            // refund buyer
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } else {
+            // pay seller
+            payable(seller).transfer(address(this).balance);
+        }
+    }
+
+    // let the smart contract receive ETH
+    receive() external payable {}
+    
+    // get balance of the contract
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    
 }
--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,60 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address private _owner;
+
+    constructor() ERC721("Real Estate", "REAL") {}
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// contract RealEstate is ERC721URIStorage {
+//     using Counters for Counters.Counter;
+//     Counters.Counter private _tokenIds;
+//     address private _owner;
+
+//     constructor() ERC721("RealEstate", "RE") {
+//         _owner = msg.sender;
+//     }
+
+//     function mint(address recipient, string memory tokenURI) public {
+//         require(msg.sender == _owner, "Only owner can mint");
+//         _tokenIds.increment();
+//         uint256 newTokenId = _tokenIds.current();
+//         _mint(recipient, newTokenId);
+//         _setTokenURI(newTokenId, tokenURI);
+//     }
+// }
